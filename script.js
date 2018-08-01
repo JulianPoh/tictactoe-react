e5c6c1ed105de10d2ab8233cd94f781981ce38cf
JulianPoh <julianpoh01@gmail.com>
2018-08-01 22:41:23 +0800

    save changes

--- script.js
+++ script.js
@@ -37,6 +37,32 @@ class Board extends React.Component {
     }
 }
 
+class Play extends React.Component {
+
+  constructor(){
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (event, colIndex) => {
+    // access to event.target here
+    console.log(event);
+  }
+
+  // our click method
+  handleClick(){
+    
+  }
+
+  render() {
+      return (
+        <button type="checkbox" onClick={((ev) => this.handleClick(ev, colIndex))}>
+            X
+        </button>
+      );
+  }
+}
+
 ReactDOM.render(
     <Board/>,
     document.getElementById('root')
